9acc237df547df67cb7f0cfbc413ca0d14029610
unamednada <dias.gustavo.dias@gmail.com>
2021-11-01 19:34:31 -0300

    Create shouldFetchposts step-by-step

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,13 +1,35 @@
 import React from "react";
 import RedditContext from "./RedditContext";
+import { getPostsBySubreddit } from "../services/redditAPI";
 
 class Provider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      isFetching: false,
+      shouldRefreshSubreddit: false,
+      selectedSubreddit: 'reactjs',
+      postsBySubreddit: {
+        frontend: {},
+        reactjs: {},
+      },
     }
   }
+
+  shouldFetchPosts() {
+    const {
+      selectedSubreddit,
+      postsBySubreddit,
+      isFetching,
+      shouldRefreshSubreddit,
+    } = this.state;
+    const posts = postsBySubreddit[selectedSubreddit];
+
+    if (!posts.items) return true;
+    if (isFetching) return false;
+    return shouldRefreshSubreddit;
+  }
+
   render() {
     const { children } = this.props;
     const context = {};
